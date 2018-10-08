f835bf6b839886ef3462bd60d8deaed4edb002e5
Paul Besret <paul.besret@hotmail.fr>
2018-10-08 02:18:27 +0200

    Add files via upload
    updated game of life implementation

--- GameOfLife/PaulBesret/GameOfLife/src/com/paulbesret/gameoflife/CellType.java
+++ GameOfLife/PaulBesret/GameOfLife/src/com/paulbesret/gameoflife/CellType.java
@@ -1,9 +1,8 @@
 package com.paulbesret.gameoflife;
 
-// vleroy - L'usage d'une énumération est un bon choix.
 public enum CellType {
 
     Populated,
-    Unpopulated
+    Unpopulated;
 
 }
--- GameOfLife/PaulBesret/GameOfLife/src/com/paulbesret/gameoflife/GridManager.java
+++ GameOfLife/PaulBesret/GameOfLife/src/com/paulbesret/gameoflife/GridManager.java
@@ -1,45 +1,63 @@
 package com.paulbesret.gameoflife;
 
-public class GridManager {
+public final class GridManager {
 
-    /* vleroy
-     * Pourquoi tes variables rows et columns sont-elles des variables de classes ? Tu les manipules comme des variables
-     * d'instance dans ton code.
-     * Ta variable grid n'est jamais utilisée. Tu te sers toujours de la variable locale du Main
-     */
-    private static int rows;
-    private static int columns;
-    private static Cell[][] grid;
+    private static int rows = 10;
+    private static int columns = 10;
+    private static Cell[][] grid = initialize();
 
-    public static int getRows() {
-        return rows;
-    }
-
-    public void setRows(int rows) {
-        GridManager.rows = rows;
-    }
-
-    public static int getColumns() {
-        return columns;
-    }
-
-    public void setColumns(int columns) {
-        GridManager.columns = columns;
+    public static void run(){
+        Cell[][] tempGrid = duplicate(grid);
+        for (int i = 1; i < rows - 1; i++) {
+            for (int j = 1; j < columns - 1; j++){
+                int populatedNeighbors = grid[i][j].checkNeighbors(grid);
+                if(grid[i][j].getType() == CellType.Populated){
+                    if(populatedNeighbors == 1 || populatedNeighbors == 0){
+                        // die
+                        tempGrid[i][j].setType(CellType.Unpopulated);
+                    } else if (populatedNeighbors >= 4){
+                        // die
+                        tempGrid[i][j].setType(CellType.Unpopulated);
+                    } else if (populatedNeighbors == 2 || populatedNeighbors == 3){
+                        // Survive
+                        tempGrid[i][j].setType(CellType.Populated);
+                    }
+                } else if(populatedNeighbors == 3){
+                    // born
+                    tempGrid[i][j].setType(CellType.Populated);
+                }
+            }
+        }
+        System.out.println();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e){
+            System.err.println(e);
+        }
+        grid = tempGrid;
+        print(grid);
     }
 
     // Initialize a grid of cells
-    public Cell[][] initialize(){
+    private static Cell[][] initialize(){
         Cell[][] grid = new Cell[rows][columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++){
                 grid[i][j] = new Cell(i,j);
             }
         }
+
+        GridManager.setCellContent(grid,4,5, CellType.Populated);
+        GridManager.setCellContent(grid,5,5, CellType.Populated);
+        GridManager.setCellContent(grid,5,6, CellType.Populated);
+        GridManager.setCellContent(grid,5,4, CellType.Populated);
+        GridManager.setCellContent(grid,6,5, CellType.Populated);
+        print(grid);
         return grid;
     }
 
     // Duplicate a grid
-    public Cell[][] duplicate(Cell[][] grid){
+    private static Cell[][] duplicate(Cell[][] grid){
         Cell[][] duplicatedGrid = new Cell[rows][columns];
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++){
@@ -51,17 +69,17 @@ public class GridManager {
     }
 
     // Print the content of a grid
-    public void print(Cell[][] grid){
+    private static void print(Cell[][] grid){
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++){
-                System.out.print(grid[i][j].getType() == CellType.Populated ? "O" : "-");
+                System.out.print(grid[i][j].getType() == CellType.Populated ? "O" : "X");
             }
             System.out.println();
         }
     }
 
     // Setup content of a given cell
-    public void setCellContent(Cell[][] grid, int x, int y, CellType type){
+    private static void setCellContent(Cell[][] grid, int x, int y, CellType type){
         grid[x][y].setType(type);
     }
 }
